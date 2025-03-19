28f5c6dc7c83cf0e567614a5ae667e85aad5f29d
Vikas <166400180+NewonGithubVikas@users.noreply.github.com>
2025-03-19 15:10:09 +0530

    Update Settings.js

--- src/component/Client/Setting/Settings.js
+++ src/component/Client/Setting/Settings.js
@@ -19,6 +19,7 @@ const Settings = () => {
           <FaHistory className="me-3 text-warning" size={24} /> 
           <span className="fs-5">Bet History</span>
         </Link>
+  
         <Link to="/refer-earn" className="list-group-item list-group-item-action d-flex align-items-center py-3">
           <FaGift className="me-3 text-warning" size={24} /> 
           <span className="fs-5">Refer & Earn</span>
